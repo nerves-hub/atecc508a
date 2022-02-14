9582349be05e2d070cf9f07c887ae10c864c5195
Jon Carstens <jjcarstens@me.com>
2022-02-14 12:55:14 -0700

    ignore dialyzer record warnings

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -10,5 +10,9 @@
   {"lib/atecc508a/certificate.ex:37:unknown_type Unknown type: :public_key.ec_private_key/0."},
   {"lib/atecc508a/certificate/template.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate/template.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {" lib/atecc508a/certificate/template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."}
+  {" lib/atecc508a/certificate/template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
+  {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
+  {"lib/atecc508a/certificate/nerves_key_template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/atecc508a/certificate/trust_and_go_template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."}
 ]
