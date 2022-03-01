8794163e6bf1f914de41c7dd7b50e1f4749d2c2f
Digit <aaron.shea@smartrent.com>
2022-03-01 13:31:33 -0800

    remove un-needed dialyzer ignores

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,5 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/atecc508a/certificate/template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
   {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
   {"lib/atecc508a/certificate.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
@@ -8,9 +7,6 @@
   {"lib/atecc508a/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
   {"lib/atecc508a/certificate.ex:33:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate.ex:37:unknown_type Unknown type: :public_key.ec_private_key/0."},
-  {"lib/atecc508a/certificate/template.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/atecc508a/certificate/template.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {" lib/atecc508a/certificate/template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
   {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
   {"lib/atecc508a/certificate/nerves_key_template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
