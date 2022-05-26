9f0ebde9d055de524594c9624312410c55a700af
Jon Carstens <jjcarstens@me.com>
2022-05-26 07:15:39 -0600

    Ignore X509 dialyzer record warnings

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,10 @@
 # Run `mix dialyzer --format short` for strings
 [
+  {"lib/x509/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/certificate/extension.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/certificate/validity.ex:15:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
   {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
   {"lib/atecc508a/certificate.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
