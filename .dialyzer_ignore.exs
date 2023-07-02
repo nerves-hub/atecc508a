2350a4d5f4e48dcd877fc4dd4d7d516a1e4260ac
Frank Hunleth <fhunleth@troodon-software.com>
2023-07-01 21:19:04 -0400

    Refresh the Dialyzer ignore file

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,19 +1,10 @@
 # Run `mix dialyzer --format short` for strings
 [
-  {"lib/x509/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/x509/certificate/extension.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/x509/certificate/validity.ex:15:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/atecc508a/certificate.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {"lib/atecc508a/certificate.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/atecc508a/certificate.ex:15:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/atecc508a/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
   {"lib/atecc508a/certificate.ex:33:unknown_type Unknown type: :public_key.ec_public_key/0."},
   {"lib/atecc508a/certificate.ex:37:unknown_type Unknown type: :public_key.ec_private_key/0."},
-  {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
-  {"lib/atecc508a/certificate/nerves_key_template.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."},
-  {"lib/atecc508a/certificate/nerves_key_template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."},
-  {"lib/atecc508a/certificate/trust_and_go_template.ex:10:unknown_type Unknown type: X509.ASN1.record/1."}
+  {"lib/x509/certificate.ex:20:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/certificate/extension.ex:12:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/certificate/validity.ex:15:unknown_type Unknown type: X509.ASN1.record/1."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.ec_public_key/0."},
+  {"lib/x509/public_key.ex:9:unknown_type Unknown type: :public_key.rsa_public_key/0."}
 ]
